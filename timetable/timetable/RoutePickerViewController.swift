fe38af68164cec3b99f951c99215040efb0f3822
Alexander Terekhov <alexnader.terekh@gmail.com>
2016-11-05 16:21:34 +0300

    added route picker controller

---
+++ timetable/timetable/RoutePickerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RoutePickerViewController.swift
+//  timetable
+//
+//  Created by Alexander on 05/11/2016.
+//  Copyright © 2016 Alexander Terekhov. All rights reserved.
+//
+
+import Foundation
+
+class RoutePickerViewController: NSObject
+{
+    override init()
+    {
+        super.init()
+    }
+    
+    
+    
+    
+}
